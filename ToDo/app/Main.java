18aa86be32ddf3bcb13d5f39e8e79d2376e1fb34
João Ravache <joaobravache@gmail.com>
2025-10-30 20:28:43 -0300

    ???

--- ToDo/app/Main.java
+++ ToDo/app/Main.java
@@ -2,14 +2,42 @@ package ToDo.app;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import ToDo.model.Tasks;
+
+
 public class Main{
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList tasks = new ArrayList<>();
+        // <Tasks> prevent that anyother thing other than tasks will be added to the list
+        ArrayList<Tasks> tasks = new ArrayList<>();
 
-        
+        Boolean newTask = true;
+
+        while(newTask){
+
+            System.out.println("Would you like to add a task? (y/n)");
+            String response = sc.nextLine();
+
+            newTask = response.equalsIgnoreCase("y") || response.equalsIgnoreCase("Y");
+
+            if(newTask){
+
+                System.out.println("Provide the name of the task: ");
+                String id = sc.nextLine();
+                System.out.println("Provide a discripiton for the task: ");
+                String discription = sc.nextLine();
+                System.out.println("Set the date that it is being added to the list (dd/mm/yy): ");
+                String createdAt = sc.nextLine();
+
+            }
+
+        }
+
+
+
+
 
         
         
--- ToDo/model/Tasks.java
+++ ToDo/model/Tasks.java
@@ -1,44 +1,20 @@
 package ToDo.model;
 
+import java.util.UUID;
+
 public class Tasks {
     
-    String id;
+    String id = UUID.randomUUID().toString();
     String description;
-    String status;
+    public enum status{
+
+        'to-do',
+        'in-progress',
+        'done'
+    }
     String createdAt; // dont really know if this is the most optimized way to set a date...
     String updatedAt; // dont really know if this is the most optimized way to set a date...
    
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getStatus() {
-        return status;
-    }
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    public String getCreatedAt() {
-        return createdAt;
-    }
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
-    public String getUpdatedAt() {
-        return updatedAt;
-    }
-    public void setUpdatedAt(String updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-    
-    
+
 
 }
