3d75feb4b7ee1bf6bd91ad1eee6df789e2e390f3
João Ravache <joaobravache@gmail.com>
2025-10-30 20:05:31 -0300

    Defining the first variables

---
+++ ToDo/app/Main.java
@@ -0,0 +1,18 @@
+package ToDo.app;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList tasks = new ArrayList<>();
+
+        
+
+        
+        
+        
+    }
+}
---
+++ ToDo/model/Tasks.java
@@ -0,0 +1,44 @@
+package ToDo.model;
+
+public class Tasks {
+    
+    String id;
+    String description;
+    String status;
+    String createdAt; // dont really know if this is the most optimized way to set a date...
+    String updatedAt; // dont really know if this is the most optimized way to set a date...
+   
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
+
+}
