708316dcf6cb4b7af744438377619d1ea51b8486
João Ravache <joaobravache@gmail.com>
2025-11-02 15:34:55 -0300

    I dont even know anymore

--- ToDo/app/Main.java
+++ ToDo/app/Main.java
@@ -1,98 +1,128 @@
 package ToDo.app;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import ToDo.status.Status;
+import ToDo.status.mainMenu;
 import ToDo.model.Tasks;
 
-
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        // <Tasks> prevent that anyother thing other than tasks will be added to the list
+        // <Tasks> prevent that any other thing other than tasks will be added to the list
         ArrayList<Tasks> tasksList = new ArrayList<>();
+        mainMenu menu = new mainMenu();
 
-        Boolean newTask = true;
+        boolean newTask = true;
 
-        while(newTask){
+        while (newTask) {
 
-            System.out.println("Would you like to add a task? (y/n)");
-            String response = sc.nextLine();
+            System.out.println(menu.getMenu());
+            System.out.println("===================");
+            System.out.print("Type your choice: ");
+            int answer = sc.nextInt();
+            sc.nextLine(); // consume enter
 
-            newTask = response.equalsIgnoreCase("y") || response.equalsIgnoreCase("Y");
+            switch (answer) {
 
-            if(newTask){
+                case 1:
 
-                System.out.println("Provide a descripiton for the task: ");
-                String description = sc.nextLine();
-                LocalDateTime createdAt = LocalDateTime.now();
-                System.out.println("Date that has being seted for: " + createdAt);
-                String statusOptions = """
-                        Choose a task status:
-                        1. To Do;
-                        2. In Progress;
-                        3. Done
-                        """;
+                    boolean addMore = true;
 
-                System.out.println(statusOptions);
-                int choice = sc.nextInt();
+                    while (addMore) {
 
-                Status status = null;
+                        System.out.println("Would you like to add a task? (y/n)");
+                        String response = sc.nextLine();
+                        addMore = response.equalsIgnoreCase("y") || response.equalsIgnoreCase("Y");
 
-                switch (choice){
+                        if (addMore) {
 
-                    case 1:
+                            System.out.println("Provide a description for the task: ");
+                            String description = sc.nextLine();
 
-                        //status calls Status defined as To do
-                        status = Status.TO_DO;
-                        break;
-                    case 2:
-                        status = Status.IN_PROGRESS;
-                        break;
-                    case 3:
-                        status = Status.DONE;
-                        break;
-                
-                    default:
-                        System.out.println("Invalide choice setting as defaul: To Do.");
-                        status = Status.TO_DO; // default case.
-                        break;
-                }
+                            LocalDateTime createdAt = LocalDateTime.now();
+                            System.out.println("Date that has been set for: " + createdAt);
 
-                //creats the task with its own description
-                Tasks task = new Tasks(description);
-                //sets task's status
-                task.setStatus(status);
-                //adds task
-                tasksList.add(task);
-                //consumes a remanecent enter
-                sc.nextLine();
+                            String statusOptions = """
+                                    Choose a task status:
+                                    1. To Do
+                                    2. In Progress
+                                    3. Done
+                                    """;
 
+                            System.out.println(statusOptions);
+                            int choice = sc.nextInt();
+                            sc.nextLine(); // consume enter
 
+                            Status status = null;
 
+                            switch (choice) {
+
+                                case 1:
+                                    status = Status.TO_DO;
+                                    break;
+                                case 2:
+                                    status = Status.IN_PROGRESS;
+                                    break;
+                                case 3:
+                                    status = Status.DONE;
+                                    break;
+                                default:
+                                    System.out.println("Invalid choice. Setting as default: To Do.");
+                                    status = Status.TO_DO;
+                                    break;
+                            }
+
+                            // creates the task with its description
+                            Tasks task = new Tasks(description);
+                            // sets task's status
+                            task.setStatus(status);
+                            // adds task
+                            tasksList.add(task);
+                        }
+                    }
+                    break;
+
+                case 2:
+
+                    
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                default:
+                    break;
             }
 
-        }
-
-        System.out.println("====== Your To-Do List ======");
-        if(tasksList.isEmpty()){
-            System.out.println("You dont have any task.");
-        }else{
-            //loop for-each
-            for(Tasks t: tasksList){
-                System.out.println("");
-                System.out.println("Description: " + t.getDescription());
-                System.out.println("Status: " + t.getStatus());
-                System.out.println("Added: " + t.getCreatedAt());
-                if(!t.getUpdatedAt().isEqual(t.getCreatedAt())){
-                    System.out.println("Updated: " + t.getUpdatedAt());
+            /* 
+            System.out.println("====== Your To-Do List ======");
+            if (tasksList.isEmpty()) {
+                System.out.println("You don't have any task.");
+            } else {
+                // loop for-each
+                for (Tasks t : tasksList) {
+                    System.out.println("");
+                    System.out.println("Description: " + t.getDescription());
+                    System.out.println("Status: " + t.getStatus());
+                    System.out.println("Added: " + t.getCreatedAt());
+                    if (!t.getUpdatedAt().isEqual(t.getCreatedAt())) {
+                        System.out.println("Updated: " + t.getUpdatedAt());
+                    }
                 }
-
             }
+            */
         }
 
+        sc.close();
     }
 }
---
+++ ToDo/status/mainMenu.java
@@ -0,0 +1,18 @@
+package ToDo.status;
+
+public class mainMenu {
+
+    private String menu = """
+
+            1. Add a task
+            2. Update task status
+            3. See To-do list
+            4. Leave
+            """;
+
+    public String getMenu() {
+        return menu;
+    }
+
+    
+}
